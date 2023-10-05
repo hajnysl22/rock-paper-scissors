328fcba448d14777ac9fc94e2d24622dc463d00c
hajnysl22 <hajnysl22@spstrutnov.cz>
2023-10-05 17:42:17 +0200

    Vyjádření číselné hodnoty textem (pro protihráče)

--- rps.py
+++ rps.py
@@ -3,3 +3,11 @@ print("Kámen, nůžky a papír")
 import random
 first = str(input("Zadejte Kámen, Nůžky nebo Papír: "))
 second = random.randint(1,3)
+
+#CISLA
+if second == 1:
+    second = "Kámen"
+elif second == 2:
+    second = "Nůžky"
+elif second == 3:
+    second = "Papír"
